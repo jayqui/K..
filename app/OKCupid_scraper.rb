1b247ae99976616ddb27748d67e0462395f43eb0
Jay Quigley <jay@coverhound.com>
2017-07-06 23:42:31 -0700

    refactor OKCupidScraper class

--- app/OKCupid_scraper.rb
+++ app/OKCupid_scraper.rb
@@ -11,10 +11,10 @@ class OKCupidScraper
   end
 
   def login
-    login_page = @scraper.get 'https://www.okcupid.com/login'
+    login_page = scraper.get('https://www.okcupid.com/login')
     login_form = login_page.form_with(id: 'loginbox_form')
-    login_form.field_with(id: 'login_username').value = @username
-    login_form.field_with(id: 'login_password').value = @password
+    login_form.field_with(id: 'login_username').value = username
+    login_form.field_with(id: 'login_password').value = password
     login_form.submit
   end
 
@@ -23,14 +23,17 @@ class OKCupidScraper
 
     screen_names.uniq.each do |screen_name|
       begin
-        html_file = @scraper.get "https://www.okcupid.com/profile/#{screen_name}?cf=regular,matchsearch"
-        dirname = make_html_directory!
-        html_file.save!("#{dirname}/#{html_file.title.gsub(/ \/ /,'_').sub('OkCupid','')}.html")
+        html_file = get_profile(screen_name)
+        dirname = make_directory!
+        html_file.save!("#{dirname}/#{filename(html_file)}")
+
         sleep rand(0.25)
-        p "Collecting data for #{screen_name}"
+        puts "Collecting data for #{screen_name}"
+
         CSVCreator.add_rows_to_csv_file!([html_file])
       rescue
-        p "No data for #{screen_name}"
+        puts "No data for #{screen_name}"
+
         CSVCreator.create_error_row!(screen_name)
         next
       end
@@ -39,8 +42,19 @@ class OKCupidScraper
 
   private
 
+  attr_reader :scraper, :username, :password
 
-  def make_html_directory!
+  def get_profile(screen_name)
+    scraper.get(
+      "https://www.okcupid.com/profile/#{screen_name}?cf=regular,matchsearch"
+    )
+  end
+
+  def filename(html_file)
+    "#{html_file.title.gsub(/ \/ /,'_').sub('OkCupid','')}.html"
+  end
+
+  def make_directory!
     array = FileUtils.mkdir_p("#{__dir__}/../html/#{YEAR}.#{MONTH}")
     array[0]
   end
